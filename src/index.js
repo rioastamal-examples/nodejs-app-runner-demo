f1d00cd7fc89e8750a41300d9ba9b306a9b23102
Rio Astamal <me@rioastamal.net>
2022-04-20 16:35:19 +0000

    Add Express framework and DynamoDB SDK

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { DynamoDBClient, GetItemCommand, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+const express = require("express");
+
+const app = express();
+const ddbclient = new DynamoDBClient({ region: process.env.APP_REGION || 'us-east-1' });
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
+const tableName = process.env.APP_TABLE_NAME || 'astamal-serverless-demo-5zzpka';
+
+// Allow CORS
+app.use((req, res, next) => {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true
+  });
+  
+  next();
+});
+
+app.use(express.json());
+
+app.get('/ping', function(req, res) {
+  res.json({ message: 'pong' });
+});
+
+const port = process.env.NODE_PORT || 8080;
+app.listen(port, () => {
+  console.log(`API running on port ${port}`);
+});
